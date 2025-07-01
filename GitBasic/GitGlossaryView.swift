d6bc4330ff89be41f60b4918dff6833a374e4d42
wnnrrr <winnerpardede05@gmail.com>
2025-07-01 15:44:34 +0700

    add 12 Git terms.

--- GitBasic/GitGlossaryView.swift
+++ GitBasic/GitGlossaryView.swift
@@ -26,6 +26,105 @@ struct GitGlossaryView: View {
         1. Click Integrate > Commit
         2. Type the commit message
         e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Repository",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Stage",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Push",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Pull",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Pull request",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Merge",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Conflict",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Remote",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Clone",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Branch",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
+        """),
+        
+        GitTerm(title: "Stash",
+                explanation: "A commit is a snapshot of your project at a specific time.",
+                example: "git commit -m \"Added login button\"",
+                howTo: """
+        1. Click Integrate > Commit
+        2. Type the commit message
+        e. sdla,dlsa
         """)
     ]
     
